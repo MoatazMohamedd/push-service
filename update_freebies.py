ca8e1193ce35cfb60a80c74ac654f863d287f286
MoatazMohamedd <moataz.mohamed@ymail.com>
2025-09-08 00:41:08 +0300

    Update update_freebies.py

--- update_freebies.py
+++ update_freebies.py
@@ -249,10 +249,10 @@ def main():
             if igdb_data:
                 enriched_games.append({**gp_game, **igdb_data})
         old_ids = {g["gamerpower_id"] for g in old_list}
-        #for game in enriched_games:
-            #if game["gamerpower_id"] not in old_ids:
-                #send_fcm_notification(game)
-        firestore_client.collection("freebies_test").document("games").set({"games": enriched_games})
+        for game in enriched_games:
+            if game["gamerpower_id"] not in old_ids:
+                send_fcm_notification(game)
+        firestore_client.collection("freebies").document("games").set({"games": enriched_games})
         write_local_json(gp_games)
         print(f"Saved {len(enriched_games)} strict-match games to Firestore.")
     else:
